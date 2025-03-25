fe63f98d2e7310291a99f04a861337a5922caf30
johnAyodele1 <ayodele.j@ihealthwellness.org>
2025-03-25 23:26:46 +0100

    Initial commit

---
+++ blinkit/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import blinkit from "./vite.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src={blinkit} alt="BlinkIt logo" />
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
